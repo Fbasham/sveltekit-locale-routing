f8780f1a33ae042e9fdcea8ceab9be517d1d2c3d
Frank Basham <frankabasham@gmail.com>
2023-05-21 16:32:06 -0400

    prep for gh-pages

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,3 +1,5 @@
+export const prerender = true;
+
 import { loadTranslations } from "$lib/translations";
 
 export const load = async ({ url }) => {
@@ -5,7 +7,7 @@ export const load = async ({ url }) => {
   let locale = pathname.split`/`[1];
   let path = "/" + pathname.split`/`.slice(2).join`/`;
 
-  await loadTranslations(locale, "common");
+  await loadTranslations(locale, path);
 
   return { locale, path };
 };
