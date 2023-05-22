bfcc51778e96e96bd02a18e72f737644882735ca
Frank Basham <frankabasham@gmail.com>
2023-05-21 20:12:29 -0400

    small fix

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -2,13 +2,19 @@ export const prerender = true;
 
 import { loadTranslations } from "$lib/translations";
 import { base } from "$app/paths";
+import { error } from "@sveltejs/kit";
 
 export const load = async ({ url }) => {
   let { pathname } = url;
   let locale = pathname.split`/`[base === "" ? 1 : 2];
   let path = "/" + pathname.split`/`.slice(base === "" ? 2 : 3).join`/`;
 
-  await loadTranslations(locale, path);
+  try {
+    await loadTranslations(locale, path);
+  } catch (e) {
+    console.log(e.message);
+    throw error(408, e.message);
+  }
 
   return { locale, path };
 };
